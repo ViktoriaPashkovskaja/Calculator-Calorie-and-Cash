6560e7837018d91e4421900f59e09319bef5970b
ViktoriaPashkovskaja <pashkovskajava@yandex.ru>
2021-05-25 14:18:05 +0300

    Повторная первая попытка

--- homework.py
+++ homework.py
@@ -3,83 +3,83 @@ from typing import List, Optional
 
 class Calculator:
     """Создание класса Calculator"""
-    def __init__(self, limit: float)->None:#Создаем limit, функция ничего не возвращает
-        self.limit = limit #создаем limit
-        self.records:List = []#создаем список records, в котором будем хранить запись
-        print(f'Ваш лимит:{limit}!') #выводим limit
+    def __init__(self, limit: float)->None:
+        self.limit = limit
+        self.records:List = []
+        print(f'Ваш лимит:{limit}!') 
 
-    def add_record(self, rec)->None: #создаем rec, функция ничего не возвращает
-        self.records.append(rec)# В список records добавляем то, что есть в rec
-        print(f'Количество: {rec.amount}, с комментарием {rec.comment} на {rec.date} ') #выводим сумму, комментарий, дату
+    def add_record(self, rec)->None: 
+        self.records.append(rec)
+        print(f'Количество: {rec.amount}, с комментарием {rec.comment} на {rec.date} ') 
 
-    def get_today_status(self)->float: #создаем функцию для расчета затрат за день, возвращаем float
+    def get_today_status(self)->float:
         """Расчет расходов за день"""
-        today_status:float = 0 #создаем переменную today_status для расчета затрат за день
-        now = dt.date.today() #создаем переменную now, чтобы брать дату за сегодня
+        today_status:float = 0 
+        now = dt.date.today() 
         
-        for rec in self.records: #создаем цикл, берём то, что у нас в списке и передаем в переменную rec
-            if rec.date == now:# Если переданная дата rec.date равна дате сегодняшней дате то...
-                today_status += rec.amount #в today_status прибавляют затраты за сегодня rec.amount
+        for rec in self.records: 
+            if rec.date == now:
+                today_status += rec.amount 
     
-        return today_status #вернули значение today_status
+        return today_status
                 
-    def get_week_stats(self)->float: #создаем функцию для расчета затрат за последние 7 дней
+    def get_week_stats(self)->float:
         """Данные за неделю""" 
-        count_cash_week:float = 0 #создаем переменную для сохранения затрат за 7 дней
-        today = dt.datetime.today().date() #создаем переменную today для сохранения текущего дня
-        day_week_ago = (dt.datetime.today().date() - dt.timedelta(days=7)) #создаем переменную day_week_ago для сохранения дней
+        count_cash_week:float = 0
+        today = dt.datetime.today().date()
+        day_week_ago = (dt.datetime.today().date() - dt.timedelta(days=7))
        
-        for rec in self.records:#Создаем цикл для учета затрат за неделю
-            if today >= rec.date >= day_week_ago:#Если сегодня дата больше или равна переданной дате или переданная дата больше или равна продолжительности, разницы между датами
-                count_cash_week += rec.amount#Подсчет затрат за неделю
-        return count_cash_week#вернули значение count_cash_week       
+        for rec in self.records:
+            if today >= rec.date >= day_week_ago:
+                count_cash_week += rec.amount
+        return count_cash_week    
     
-class CaloriesCalculator(Calculator):#Создаем класс для определения остатка каллорий
-    def get_calories_remained(self): #Создаем функцию для определения остатка каллорий
-        calories_amount = self.get_today_status() #Создаем переменную для каллорий за сегодня
-        if calories_amount < self.limit: #Если каллории больше или равно чем установлено
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        calories_amount = self.get_today_status()
+        if calories_amount < self.limit: 
             print(f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {self.limit - calories_amount} кКал»')
         else:
             print('Хватит есть!')
 
-class CashCalculator(Calculator): #Создаем класс для определения остатка денег
+class CashCalculator(Calculator):
     """Калькулятор для денег. Показывает в трех валютах потраченные деньги за день и неделю"""   
-    USD_RATE = float(73.59) #Создаем константу доллара
-    EURO_RATE = float(89.78) #Создаем константу евро
-    RUB_RATE = 1.0 #Создаем константу рубля
+    USD_RATE = float(73.59)
+    EURO_RATE = float(89.78) 
+    RUB_RATE = float(1)
     
-    def get_today_cash_remained(self, currency): #Создаем функцию get_today_cash_remained для расчета остатка денег на сегодня
+    def get_today_cash_remained(self, currency): 
         
-        today_cash: float = self.get_today_status()#Создаем переменную today_cash для сохранения затрат за денб
+        today_cash: float = self.get_today_status()
  
-        if currency == 'usd': #Проверяем валюту
-            curr:str = 'USD' #Присваеваем валюту
-            balance = round((self.limit - today_cash) / self.USD_RATE, 2) #Баланс в долларах в день
-        elif currency == 'eur': #Проверяем валюту
-            curr = 'EUR' #Присваеваем валюту
-            balance = round((self.limit - today_cash) / self.EUR_RATE, 2) #Баланс в евро в день
-        elif currency == 'rub': #Проверяем валюту
-            curr = 'RUB' #Присваеваем валюту
-            balance = round((self.limit - today_cash) / self.RUB_RATE, 2) #Баланс в рублях в день
+        if currency == 'usd': 
+            curr:str = 'USD'
+            balance = round((self.limit - today_cash) / self.USD_RATE, 2) 
+        elif currency == 'eur':
+            curr = 'EUR'
+            balance = round((self.limit - today_cash) / self.EUR_RATE, 2) 
+        elif currency == 'rub':
+            curr = 'RUB'
+            balance = round((self.limit - today_cash) / self.RUB_RATE, 2)
         else:
-            currency = '' #Нет валюты
+            currency = ''
             print('Нет такой валюты')
         
-        if today_cash<self.limit: #Остаток на день, деньги остались
+        if today_cash<self.limit:
             money_days:float = 'На сегодня осталось ' + str(balance) + ' ' + curr
-        elif today_cash == self.limit: #Остаток на день, деньги по 0
+        elif today_cash == self.limit:
             money_days = 'Денег нет, держись'
-        else: #Остаток на день, нет денег
+        else:
             money_days = 'Денег нет, держись: твой долг - ' + str(balance) + ' ' + curr
         return money_days
     
-class Record: #Класс для создания записей
+class Record:
     """Класс для создания записей"""
-    def __init__(self, amount:float, comment:str, date:Optional[str] = None)->None: #Функция для создания записей
-        self.amount = amount #Деньги или каллории
-        self.comment = comment #Комментарии
-        date_format = '%d.%m.%Y' #Формат даты
-        if date is None: #Проверка даты, сегодняшняя или нет
+    def __init__(self, amount:float, comment:str, date:Optional[str] = None)->None:
+        self.amount = amount
+        self.comment = comment
+        date_format = '%d.%m.%Y'
+        if date is None:
             self.date = dt.datetime.today().date()
         else:
             self.date = dt.datetime.strptime(date, date_format).date()
